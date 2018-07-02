4c5f0c529aa6fd38d1ca41402aab7567e93b6fb4
Eric Mendes Dantas <ericdantas0@gmail.com>
2018-07-02 15:12:08 -0300

    .

--- main.go
+++ main.go
@@ -2,10 +2,21 @@ package main
 
 import (
 	"fmt"
-	"crypto/sha256"
+
+	"github.com/ericmdantas/go-block/blockchain"
 )
 
 func main() {
-	sum := sha256.Sum256([]byte("shhhh\n"))
-	fmt.Printf("%x", sum)
+	bc := blockchain.NewBlockchain()
+
+	bc.AddBlock("wat 1")
+	bc.AddBlock("wat 2")
+	bc.AddBlock("wat 3")
+
+	for _, block := range bc.Blocks {
+		fmt.Printf("PrevHash: %x\n", block.PrevBlockHash)
+		fmt.Printf("Data: %x\n", block.Data)
+		fmt.Printf("Hash: %x\n", block.Hash)
+		fmt.Println()
+	}
 }
